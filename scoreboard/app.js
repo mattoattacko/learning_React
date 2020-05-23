95a0220a71195bc1b73a3fa4e67187e6937d9b1a
Matto <matt.mcquain@gmail.com>
2020-05-22 19:22:29 -0700

    implimenting jsx

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,5 +1,15 @@
-const title = React.createElement(
-  'h1',
-  { id:'main-title', title:'Title here' },
-  'My First React Element!'
+const desc = 'React node rendered to the DOM';
+const myTitleID = 'main-title';
+const name = 'Matt';
+
+const header = (
+  <header>
+    <h1 id={myTitleID}>{ name }'s First React Element</h1>
+    <p>{ desc }</p>
+  </header>
+);
+
+ReactDOM.app.render(
+  header,
+  document.getElementById('root')
 );
