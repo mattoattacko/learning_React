da81985649b3f777c585431aab852418c6625a7f
Matto <matt.mcquain@gmail.com>
2020-05-23 22:19:26 -0700

    modularized top level element

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -7,7 +7,36 @@ const Header = () => {
   );
 }
 
+const Player = () => {
+  return (
+    <div className="player">
+      <span className="player-name">Matto</span>
+    </div>
+  );
+};
+
+const Counter = () => {
+  return (
+    <div className="counter">
+      <button className="counter-action decrement"> - </button>
+      <span className="counter-score">21</span>
+      <button className="counter-action increment"> + </button>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="scoreboard">
+      <Header />
+
+      {/* Players list  */}
+      <Player />
+    </div>
+  );
+}
+
 ReactDOM.app.render(
-  <Header />,
+  <App />,
   document.getElementById('root')
 );
