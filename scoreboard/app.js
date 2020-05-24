701301fe929472e22a1a8ff8d3f4a8862622143a
Matto <matt.mcquain@gmail.com>
2020-05-23 19:37:44 -0700

    converting to components

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -1,13 +1,11 @@
-const desc = 'React node rendered to the DOM';
-const myTitleID = 'main-title';
-const name = 'Matt';
-
-const header = (
-  <header>
-    <h1 id={myTitleID}>{ name }'s First React Element</h1>
-    <p>{ desc }</p>
-  </header>
-);
+function Header() {
+  return (
+    <header>
+      <h1>Scoreboard</h1>
+      <span className="stats">Players: 1</span>
+    </header>
+  );
+}
 
 ReactDOM.app.render(
   header,
