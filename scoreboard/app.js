40621d7ae9e5abf3c2d2b1f7dec97f0370875b6d
Matto <matt.mcquain@gmail.com>
2020-05-26 22:42:18 -0700

    wiring buttons

--- scoreboard/app.js
+++ scoreboard/app.js
@@ -41,12 +41,20 @@ const Player = (props) => {
 };
 
 class Counter extends React.Component {
+  state = {
+    score: 0
+  };
+
+  incrementScore() {
+    
+  }
+
   render() {
     return (
       <div className="counter">
         <button className="counter-action decrement"> - </button>
-        <span className="counter-score">{ this.props.score }</span>
-        <button className="counter-action increment"> + </button>
+        <span className="counter-score">{ this.state.score }</span>
+        <button className="counter-action increment" onClick={this.incrementScore}> + </button>
       </div>
     );
   }
